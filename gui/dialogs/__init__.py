5c73972bdfe2c9403048aeb7b3c609a43623bc34
dmitri-0 <141411224+dmitri-0@users.noreply.github.com>
2025-11-20 19:21:50 +0300

    Добавлен __init__.py для пакета dialogs

---
+++ gui/dialogs/__init__.py
@@ -0,0 +1,6 @@
+"""Пакет диалоговых окон"""
+
+from .help_dialog import HelpDialog
+from .database_settings_dialog import DatabaseSettingsDialog
+
+__all__ = ['HelpDialog', 'DatabaseSettingsDialog']
