95ac0a96be06e873db2c7bf3216cfaf04c76fa84
dmitri-0 <141411224+dmitri-0@users.noreply.github.com>
2025-11-25 23:25:54 +0300

    Export ProcessActions in gui/actions

--- gui/actions/__init__.py
+++ gui/actions/__init__.py
@@ -1,4 +1,5 @@
 from .database_actions import DatabaseActions
 from .database_operations import DatabaseOperations
+from .process_actions import ProcessActions
 
-__all__ = ['DatabaseActions', 'DatabaseOperations']
+__all__ = ['DatabaseActions', 'DatabaseOperations', 'ProcessActions']
