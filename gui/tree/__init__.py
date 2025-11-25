147f5bc6bc8e2cb01f54aabda3782662c27f7de0
dmitri-0 <141411224+dmitri-0@users.noreply.github.com>
2025-11-25 23:26:08 +0300

    Export OpenedBasesTreeBuilder in gui/tree

--- gui/tree/__init__.py
+++ gui/tree/__init__.py
@@ -1,3 +1,4 @@
 from .tree_builder import TreeBuilder
+from .opened_bases_tree_builder import OpenedBasesTreeBuilder
 
-__all__ = ['TreeBuilder']
+__all__ = ['TreeBuilder', 'OpenedBasesTreeBuilder']
