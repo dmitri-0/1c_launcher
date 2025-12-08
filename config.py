c4504444491a73449817fcb13a94af4e297ba9bb
Печерица Дмитрий Валерьевич <pechericadv@comandor.local>
2025-12-08 21:10:56 +0700

    Update from pechericadv@PROGRAM43 | 2025-12-08 21:10 | main

--- config.py
+++ config.py
@@ -6,3 +6,6 @@ IBASES_PATH = Path(os.getenv('USERPROFILE')) / 'AppData' / 'Roaming' / '1C' / '1
 
 # Кодировка файла ibases.v8i
 ENCODING = 'utf-8-sig'
+
+# Путь к обработке инструментов ИР
+IR_TOOLS_PATH = r"C:\ROOT\7_90.1\wp\Портативный.erf"
