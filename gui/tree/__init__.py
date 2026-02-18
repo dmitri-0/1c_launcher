058bdf73b87794fef867a9c5b66d3974a45c6bc0
dmitri-0 <141411224+dmitri-0@users.noreply.github.com>
2026-02-18 19:51:13 +0700

    Add MainProcessesTreeBuilder to tree module exports

--- gui/tree/__init__.py
+++ gui/tree/__init__.py
@@ -1,4 +1,5 @@
 from .tree_builder import TreeBuilder
 from .opened_bases_tree_builder import OpenedBasesTreeBuilder
+from .main_processes_tree_builder import MainProcessesTreeBuilder
 
-__all__ = ['TreeBuilder', 'OpenedBasesTreeBuilder']
+__all__ = ['TreeBuilder', 'OpenedBasesTreeBuilder', 'MainProcessesTreeBuilder']
